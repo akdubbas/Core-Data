8e73c1b2fc26033072e89911e6f3f9b84ca12ae4
Amith <amith.dubbasi@kroger.com>
2018-05-31 00:53:54 -0400

    Adding and Deleting in Core Data stack

--- Mooskine/AppDelegate.swift
+++ Mooskine/AppDelegate.swift
@@ -20,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         dataController.load()
+        
+        let navigationController = window?.rootViewController as! UINavigationController
+        let noteboooksListViewController = navigationController.topViewController as! NotebooksListViewController
+        noteboooksListViewController.dataController = dataController
         return true
     }
 
